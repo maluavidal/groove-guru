3f5a26ecfbded22fd5a565d8512c84f4b4820ce6
Emanuel Laureano <97695747+emanuellfelipe@users.noreply.github.com>
2023-10-24 23:18:31 -0300

    Update grooveguru.py
    Novos Gráficos da Analise Exploratória

--- grooveguru.py
+++ grooveguru.py
@@ -30,13 +30,6 @@ plt.ylabel('Dancabilidade (%)')
 plt.title('Relacao entre Popularidade e Dançabilidade de Músicas')
 st.pyplot(plt.gcf())  # Adicione esta linha para exibir o gráfico no Streamlit
 
-#plt.figure(figsize=(10,6))
-#plt.scatterplot(data=banco_dados, x='acousticness_%', y='liveness_%', size='streams', sizes=(1, 100), alpha=0.5)
-#plt.xlabel('Acústica (%)')
-#plt.ylabel('Ao Vivo (%)')
-#plt.title('Relação entre Músicas Acústicas e Ao Vivo com Popularidade')
-#st.pyplot(plt.gcf())  # Adicione esta linha para exibir o gráfico no Streamlit
-
 # Criação do gráfico de dispersão
 plt.figure(figsize=(10, 6))
 
@@ -44,15 +37,66 @@ plt.figure(figsize=(10, 6))
 kmeans = KMeans(n_clusters=3)  # Número de clusters desejado
 clusters = kmeans.fit_predict(banco_dados[['liveness_%', 'acousticness_%']])
 
-# Mapeamento das cores para os clusters
-colors = ['red', 'blue', 'green']  # Defina as cores dos clusters aqui
 
-for cluster in range(3):
-    plt.scatter(banco_dados[clusters == cluster]['liveness_%'], banco_dados[clusters == cluster]['acousticness_%'], c=colors[cluster], alpha=0.5)
 
-plt.xlabel('Liveness (%)')
-plt.ylabel('Acousticness (%)')
-plt.title('Relação entre Acüstica nas Músicas e Músicas Ao Vivo')
+# Cluster Novo Criação do gráfico de dispersão Performace Ao Vivo
+plt.figure(figsize=(10, 6))
 
-# Exibição do gráfico no Streamlit
-st.pyplot(plt) 
+#Aplicação do K-Means para agrupar os dados
+kmeans = KMeans(n_clusters=5)  # Número de clusters desejado
+clusters = kmeans.fit_predict(banco_dados[['bpm', 'liveness_%']])
+
+#Mapeamento das cores para os clusters
+colors = ['blue', 'green', 'yellow', 'purple','#19FFA7']  # Defina as cores dos clusters aqui
+
+for cluster in range(5):
+    plt.scatter(banco_dados[clusters == cluster]['bpm'], banco_dados[clusters == cluster]['liveness_%'], c=colors[cluster], alpha=0.5)
+
+plt.xlabel('BPM')
+plt.ylabel('Perfomaces ao Vivo (%)')
+plt.title('Relação entre BPM e Performaces ao Vivo')
+
+#Exibição do gráfico no Streamlit   
+st.pyplot(plt)
+
+
+# Cluster Novo Criação do gráfico de dispersão Acústico
+plt.figure(figsize=(10, 6))
+
+#Aplicação do K-Means para agrupar os dados
+kmeans = KMeans(n_clusters=5)  # Número de clusters desejado
+clusters = kmeans.fit_predict(banco_dados[['bpm', 'acousticness_%']])
+
+#Mapeamento das cores para os clusters
+colors = ['red', 'green', 'blue', 'purple','#19FFA7']  # Defina as cores dos clusters aqui
+
+for cluster in range(5):
+    plt.scatter(banco_dados[clusters == cluster]['bpm'], banco_dados[clusters == cluster]['acousticness_%'], c=colors[cluster], alpha=0.5)
+
+plt.xlabel('BPM')
+plt.ylabel('Musicas Acústicas (%)')
+plt.title('Relação entre BPM e Músicas Acústicas')
+
+#Exibição do gráfico no Streamlit   
+st.pyplot(plt)
+
+
+# Cluster Novo Criação do gráfico de dispersão Instrumental
+plt.figure(figsize=(10, 6))
+
+#Aplicação do K-Means para agrupar os dados
+kmeans = KMeans(n_clusters=5)  # Número de clusters desejado
+clusters = kmeans.fit_predict(banco_dados[['bpm', 'instrumentalness_%']])
+
+#Mapeamento das cores para os clusters
+colors = ['red', 'green', 'blue', 'purple','#19FFA7']  # Defina as cores dos clusters aqui
+
+for cluster in range(5):
+    plt.scatter(banco_dados[clusters == cluster]['bpm'], banco_dados[clusters == cluster]['instrumentalness_%'], c=colors[cluster], alpha=0.5)
+
+plt.xlabel('BPM')
+plt.ylabel('Elementos Instrumentais (%)')
+plt.title('Relação entre BPM e Tipos de Elementos Instrumentais')
+
+#Exibição do gráfico no Streamlit   
+st.pyplot(plt)
