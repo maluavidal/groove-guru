eee39cfce3392208019aab2acedd5e52787218c2
Carolina-dAraujo <araujoncarolina@gmail.com>
2024-02-12 01:43:20 -0300

    adicionando data_cleaning

---
+++ Streamlit/scripts/data_cleaning.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def clean_data():
+    df = pd.read_csv('archive/all/spotify-2023.csv')
+
+    df.dropna(subset=['in_shazam_charts'], inplace=True)
+    df['in_shazam_charts'] = df['in_shazam_charts'].str.replace(',', '')
+    df['in_shazam_charts'] = df['in_shazam_charts'].astype(int)
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+
+    return df
